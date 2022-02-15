94aabe93d26dc7fe3e128829d73189b112c21c9c
tankisomokoena-dariel <tankiso.mokoena@dariel.co.za>
2022-02-15 15:21:41 +0200

    started our hackathon website

---
+++ 3-Idiots/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _3_Idiots.Controllers
+{
+	public class HomeController : Controller
+	{
+		userTablesController dbAccess = new userTablesController();
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Home()
+        {
+			return View();
+        }
+
+		public ActionResult Login(string email, string password)
+        {
+			if(dbAccess.VerifyDetails(email,password) == -1)
+            {
+				return View("~/Views/Home/Index.cshtml");
+            }
+            else
+            {
+				return View("~/Views/Home/Home.cshtml");
+			}
+        }
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
---
+++ 3-Idiots/Controllers/userTablesController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _3_Idiots;
+
+namespace _3_Idiots.Controllers
+{
+    public class userTablesController : Controller
+    {
+        private Entities1 db = new Entities1();
+
+        public int VerifyDetails(string email, string password)
+        {
+            var result = db.userTables.Where(x => x.email == email && x.password == password).FirstOrDefault();
+            if(result == null)
+            {
+                return -1;
+            }
+            else
+            {
+                return result.userID;
+            }
+           
+        }
+
+        // GET: userTables
+        public ActionResult Index()
+        {
+            return View(db.userTables.ToList());
+        }
+
+        // GET: userTables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            userTable userTable = db.userTables.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // GET: userTables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: userTables/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "userID,firstName,lastName,email,phoneNO,password")] userTable userTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.userTables.Add(userTable);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userTable);
+        }
+
+        // GET: userTables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            userTable userTable = db.userTables.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // POST: userTables/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "userID,firstName,lastName,email,phoneNO,password")] userTable userTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userTable);
+        }
+
+        // GET: userTables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            userTable userTable = db.userTables.Find(id);
+            if (userTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userTable);
+        }
+
+        // POST: userTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            userTable userTable = db.userTables.Find(id);
+            db.userTables.Remove(userTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
