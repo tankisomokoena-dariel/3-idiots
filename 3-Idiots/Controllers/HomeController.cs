9a52214ca1e1a2c50816358351f2d3a587e634d7
tankisomokoena-dariel <tankiso.mokoena@dariel.co.za>
2022-02-22 15:24:36 +0200

    added API client

--- 3-Idiots/Controllers/HomeController.cs
+++ 3-Idiots/Controllers/HomeController.cs
@@ -3,46 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IdiotsAPI;
+using _3_Idiots.Models;
 
 namespace _3_Idiots.Controllers
 {
-	public class HomeController : Controller
-	{
-        userTablesController dbAccess = new userTablesController();
-        public ActionResult Index()
-		{
-			return View();
-		}
+    public class HomeController : Controller
+    {
+        UsersClient client = new UsersClient();
 
-		public ActionResult Home()
+        public ActionResult Index()
         {
-			return View();
+            return View();
         }
 
-		public ActionResult Login(string email, string password)
+        public ActionResult Home()
         {
-            if (dbAccess.VerifyDetails(email, password) == -1)
-            {
-                return View("~/Views/Home/Index.cshtml");
-            }
-            else
+            return View();
+        }
+
+        public ActionResult Login(string email, string password)
+        {
+            Authenticate user = new Authenticate();
+            user.Email = email;
+            user.Password = password;
+            var response = client.LoginAsync(user).Result;
+            if (response != 0)
             {
                 return View("~/Views/Home/Home.cshtml");
             }
+            else
+            {
+                ViewBag.ResponseMessage = "Incorrect email or password";
+                return View("~/Views/Home/Index.cshtml");
+            }
         }
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
 
-			return View();
-		}
+            return View();
+        }
 
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
-			return View();
-		}
-	}
+            return View();
+        }
+    }
 }
---
+++ 3-Idiots/Controllers/QandAController.cs
@@ -0,0 +1,97 @@
+﻿using IdiotsAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _3_Idiots.Controllers
+{
+    public class QandAController : Controller
+    {
+        QandAClient QandAClient = new QandAClient();
+        // GET: QandA
+        public ActionResult Index()
+        {
+            int userID = 1;
+            var myQuestions = QandAClient.ViewMyQuestionsAsync(userID).Result;
+            return View(myQuestions.ToList());
+        }
+
+        // GET: QandA/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: QandA/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: QandA/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: QandA/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var entry = QandAClient.GetAsync(id).Result;
+            return View(entry);
+        }
+
+        // POST: QandA/Edit/5
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "qaID, userID, answer, question")] QandA update)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                if (ModelState.IsValid)
+                {
+                    QandAClient.UpdateMyQuestionOrAnswerAsync(update.UserID, (int)update.QaID, update).Wait();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: QandA/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: QandA/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
